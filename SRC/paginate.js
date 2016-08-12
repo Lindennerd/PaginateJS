99c23906ea62cb6577b84cab70b052222c2cdf0e
Luiz Paulo <luiz.paulo@powerpatria.corp>
2016-08-12 14:14:43 -0300

    bug fix and add params options

--- SRC/paginate.js
+++ SRC/paginate.js
@@ -3,72 +3,88 @@ function Paginate(options) {
 
     const config = extend(options, {
         source: null,
+        params: null,
         container: null,
         page: 0,
         pageSize: 10,
         total: null,
         callback: null,
-        template: '<div class="ui center aligned segment">'+
-                '    <div class="ui center aligned pagination menu">'+
-                '        <a id="goToFirstPage" class="icon item">'+
-                '            <i class="angle double left icon"></i></a>'+
-                '        <a id="goToPreviousPage" class="icon item">'+
-                '            <i class="angle left icon"></i>'+
-                '        </a>'+
-                '        <a class="ui dropdown item">Página {{page}} de {{maxPages}}</a>'+
-                '        <a id="goToNextPage" class="icon item">'+
-                '            <i class="angle right icon"></i>'+
-                '        </a>'+
-                '        <a id="goToLastPage" class="icon item">'+
-                '            <i class="angle double right icon"></i></a>'+
-                '    </div>'+
-                '</div>'
+        template: '<div class="ui center aligned segment">' +
+        '    <div class="ui center aligned pagination menu">' +
+        '        <a id="goToFirstPage" class="icon item">' +
+        '            <i class="angle double left icon"></i></a>' +
+        '        <a id="goToPreviousPage" class="icon item">' +
+        '            <i class="angle left icon"></i>' +
+        '        </a>' +
+        '        <a class="ui dropdown item">Página {{page}} de {{maxPages}}</a>' +
+        '        <a id="goToNextPage" class="icon item">' +
+        '            <i class="angle right icon"></i>' +
+        '        </a>' +
+        '        <a id="goToLastPage" class="icon item">' +
+        '            <i class="angle double right icon"></i></a>' +
+        '    </div>' +
+        '</div>'
     });
 
-    Paginate.prototype.render = function() {
-        load(function(resp) {
+    Paginate.prototype.render = function () {
+        load(function (resp) {
             var template = config.template.replace('{{page}}', config.page).replace('{{maxPages}}', maxPages());
             var el = document.querySelectorAll(config.container);
             if (!config.container) {
                 console.log('indicate a container element');
                 return;
             }
-            if (el.length > 0) {    
+            if (el.length > 0) {
                 el[0].innerHTML = template;
 
-                document.getElementById("goToFirstPage").onclick = goToFirstPage; 
-                document.getElementById("goToNextPage").onclick = goToNextPage; 
+                document.getElementById("goToFirstPage").onclick = goToFirstPage;
+                document.getElementById("goToNextPage").onclick = goToNextPage;
                 document.getElementById("goToLastPage").onclick = goToLastPage;
                 document.getElementById("goToPreviousPage").onclick = goToPreviousPage;
             }
 
-            if(config.callback){
+            if (config.callback) {
                 config.callback(resp);
             }
         });
     };
 
-    function load(callback){
+    function load(callback) {
         var request = new XMLHttpRequest();
-        var params = '?page='+config.page+'&itensPerPage='+config.pageSize;
-        request.open('GET', config.source+params, true);
+        var params = parseLoadParameters();
 
-        request.onload = function() {
-          if (request.status >= 200 && request.status < 400) {
-            var resp = JSON.parse(request.responseText);
-            config.total = resp.Total;
-            callback(resp.Items);
+        request.open('GET', config.source + params, true);
 
-          } else {
-            console.log(request.status);
-          }
+        request.onload = function () {
+            if (request.status >= 200 && request.status < 400) {
+                var resp = JSON.parse(request.responseText);
+                config.total = resp.Total;
+                callback(resp.Items);
+
+            } else {
+                console.log(request.status);
+            }
         };
 
-        request.onerror = function(err) {
+        request.onerror = function (err) {
             console.log(err);
         };
 
         request.send();
+    }
+
+    function parseLoadParameters() {
+        var params = '?page=' + config.page + '&itensPerPage=' + config.pageSize;
+
+        if (config.params) {
+            for (var param in config.params) {
+                if (config.params.hasOwnProperty(param)) {
+                    params += '&' + param + "=" + config.params[param];
+                }
+            }
+        }
+
+        return params;
     }
 
     function maxPages() {
@@ -82,28 +98,28 @@ function Paginate(options) {
         }
     };
 
-    function goToPreviousPage (callback) {
+    function goToPreviousPage(callback) {
         if (config.page > 0) {
             config.page--;
             Paginate.prototype.render();
         }
     };
 
-    function goToFirstPage (callback) {
-            config.page = 0;
-            Paginate.prototype.render();
+    function goToFirstPage(callback) {
+        config.page = 0;
+        Paginate.prototype.render();
     };
 
-    function goToLastPage (callback) {
+    function goToLastPage(callback) {
         config.page = maxPages();
         Paginate.prototype.render();
     };
 
     // custom extend function
-    function extend (opt, defaultConfig) {     
+    function extend(opt, defaultConfig) {
         var config = {};
 
-        for(var defProp in defaultConfig){
+        for (var defProp in defaultConfig) {
             if (defaultConfig.hasOwnProperty(defProp) && opt[defProp]) {
                 config[defProp] = opt[defProp];
             } else {
